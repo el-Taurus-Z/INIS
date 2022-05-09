0a4e907e44d42a8975b1347b44a3de11a312f2db
el-Taurus-Z <mazvlad0808@gmail.com>
2022-05-09 18:55:13 +0300

    add initProducts function

--- main.js
+++ main.js
@@ -4,7 +4,17 @@
 let initProducts = () => {
     // To see the shirts object, run:
     // console.log(shirts);
+document.getElementById('img1').src = shirts[0].colors.white.front;
+  document.getElementById('name1').innerHTML = shirts[0].name;
 
+  document.getElementById('img2').src = shirts[1].colors.white.front;
+  document.getElementById('name2').innerHTML = shirts[1].name;
+
+  document.getElementById('img3').src = shirts[2].colors.white.front;
+  document.getElementById('name3').innerHTML = shirts[2].name;
+
+  document.getElementById('img4').src = shirts[3].colors.white.front;
+  document.getElementById('name4').innerHTML = shirts[3].name;
     // Your Code Here
 };
 
